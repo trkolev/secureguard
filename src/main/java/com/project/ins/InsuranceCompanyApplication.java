a1b5a76169d90f81d5b3aff4a69123c542b2068c
trkolev <trkphv@gmail.com>
2025-10-08 17:38:05 +0300

    feat: Project initialization

---
+++ src/main/java/com/project/ins/InsuranceCompanyApplication.java
@@ -0,0 +1,14 @@
+package com.project.ins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceCompanyApplication.class, args);
+        System.out.println("Server Started");
+	}
+
+}
